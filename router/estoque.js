5b8c76c959068769b0c56b4ef49ab16ae7c2216f
DouradoUilquer <69002166+DouradoUilquer@users.noreply.github.com>
2020-08-19 15:05:29 -0300

    Update estoque.js
    ATUALIZAR O SALDO DO ESTOQUE

--- router/estoque.js
+++ router/estoque.js
@@ -13,12 +13,33 @@ router.get("/estoque", (req, res) => {
 
 //BUSCAR PRODUTO POR CATEGORIA
 router.post("/estoque", (req, res) => {
-    var {categoria} = req.body
+    var { categoria } = req.body
     dataBase.select('codigo', 'descricao', 'descricao_fast_food', 'preco_venda', 'categoria').where('categoria', categoria).table("estoque").then(estoque => {
         res.json({ estoque })
     }).catch(err => {
-        console.log(err)
+        res.sendStatus(404)
     })
+})
+
+//ATUALIZAR SALDO DO ESTOQUE
+router.put("/estoque", (req, res) => {
+    var itens = req.body;
+    const itensJson = itens
+    const quantidade = itensJson.length;
+    let contador = 0;
+
+    while (contador < quantidade) {
+
+        var idEstoque = itensJson[contador].id_estoque
+        var qtde = itensJson[contador].quantidade
+
+        dataBase.raw("update estoque set saldo=saldo-'" + qtde + "' where id_estoque='" + idEstoque + "' and tipo='PRODUTO'").then(() => {
+        }).catch(err => {
+            console.log(err)
+        })
+        contador++;
+    }
+    res.sendStatus(200)
 })
 
 module.exports = router;
